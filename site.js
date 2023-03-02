00685b1d37b85dac674da75214471e47c3019da2
IsabelJAR <92735909+IsabelJAR@users.noreply.github.com>
2023-03-02 14:46:34 -0500

    Milestone 4 (step 7)
    method to convert dataArray into US format, convert minutes to hours, go through poster index, and increase likes and dislikes

--- site.js
+++ site.js
@@ -41,6 +41,63 @@ const vue_app = Vue.createApp({
     },
       methods: {
             /* ADD FUNCTIONS/METHODS FOR STEP 7 HERE */
+            //function to convert teh array to USA date order
+            getMonthText(dateArray){
+                  switch (dateArray[1]) {
+                    case 1:
+                      monthName = "January";
+                      break;
+                    case 2:
+                      monthName = "February";
+                      break;
+                    case 3:
+                      monthName = "March";
+                      break;
+                    case 4:
+                      monthName = "April";
+                      break;
+                    case 5:
+                      monthName = "May";
+                      break;
+                    case 6:
+                      monthName = "June";
+                      break;
+                    case 7:
+                      monthName = "July";
+                      break;
+                    case 8:
+                      monthName = "August";
+                      break;
+                    case 9:
+                      monthName = "September";
+                      break;
+                    case 10:
+                      monthName = "October";
+                      break;
+                    case 11:
+                      monthName = "November";
+                      break;
+                    case 12:
+                      monthName = "December";
+                      break;
+                  }
+                  return monthName + ' ' + dateArray[2] + ', ' + dateArray[0];
+            },
+            posterClick(index){
+              if (
+                this.movies[index].posterindex >= 
+                this.movies[index].posters.length - 1
+              ) {
+                this.movies[index].posterindex = 0;
+              } else {
+                this.movies[index].posterindex++;
+              }
+            },
+            timeText(minutes){
+              var h = Math.floor(minutes / 60);
+              var min = minutes % 60;
+              return h + 'h ' + min + 'm';
+            }
       }
 })
 
